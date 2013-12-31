a2e7674b86dc4358ef08ede428436d8f0b12aac3
timpohladthomas <timpohladthomas@gmail.com>
2013-12-31 00:06:57 -0500

    Prepare for building

--- slim.mk
+++ slim.mk
@@ -1,22 +1,15 @@
+$(call inherit-product, vendor/slim/config/cdma.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, device/motorola/dlx/full_dlx.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Release name
-PRODUCT_RELEASE_NAME := dlx
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/htc/dlx/full_dlx.mk)
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := dlx
-PRODUCT_NAME := cm_dlx
+PRODUCT_NAME := slim_dlx
 PRODUCT_BRAND := Verizon
 PRODUCT_MODEL := HTC6435LVW
 PRODUCT_MANUFACTURER := HTC
@@ -29,3 +22,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="3.06.605.4 CL277065 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
+
+-include vendor/slim/config/common_versions.mk
